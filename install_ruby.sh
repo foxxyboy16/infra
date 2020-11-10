e1c833cb6147fc171a45a508832a8074add06199
artem <artem.ermolaev1997@gmail.com>
2020-11-10 19:21:01 +0200

    adding files

---
+++ install_ruby.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update -y
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
+ruby -v && bundle -v
+
