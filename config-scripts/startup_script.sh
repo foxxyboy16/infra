1446e65fb1c93e302e72940f902f04272dae74fe
Artem Ermolaev <artem.ermolaev1997@gmail.com>
2021-02-04 12:56:41 -0800

    commit

---
+++ config-scripts/startup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#install ruby
+apt update
+apt install -y ruby-full ruby-bundler build-essential
+
+#install mongodb
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 mu\
+ltiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt-get update -y
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
+
+#deploy
+git clone https://github.com/Artemmkin/reddit.git
+cd reddit && bundle install
+puma -d
